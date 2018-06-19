b973d0e3f5b0f45bb2dda569d02c9d369e8aee4c
Yurick <yurick.hauschild@gmail.com>
2018-06-19 16:48:32 -0300

    ✨  Add event number handler

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import 'normalize.css/normalize.css'
 import './base/code.css'
 import './style.css'
 
+import EventCover from './components/event-cover'
 import ControlPanel from './components/control-panel'
 
+EventCover()
 ControlPanel()
--- components/control-panel/index.js
+++ components/control-panel/index.js
@@ -1,10 +1,28 @@
-import './control-panel.css'
+import './style.css'
 
 export default function ControlPanel() {
-  const graphics = document.querySelector('.event-cover > .brand > svg > g')
   const panel = document.querySelector('.control-panel')
+  const cover = document.querySelector('.event-cover')
+
+  const brand = cover.querySelector('.brand')
+  const graphics = brand.querySelector('svg > g')
 
   panel.querySelector('select').onchange = function(event) {
-    graphics.style.fill = event.currentTarget.value
+    const selectedColor = event.currentTarget.value
+    const green = parseInt(selectedColor.slice(3, 5), 16)
+
+    graphics.style.fill = green > 0xA0 ? '#000' : '#fff'
+    brand.style.backgroundColor = selectedColor
+  }
+
+  const code = cover.querySelector('code')
+
+  panel.querySelector('.number > input').onkeyup = function(event) {
+    const currentValue = code.innerText
+    const number = event.currentTarget.value
+
+    code.innerHTML = PR.prettyPrintOne(`.co-de-sign__${number} {
+  date: '18/06; /* terça-feira /*'
+}`, 'css')
   }
 }
