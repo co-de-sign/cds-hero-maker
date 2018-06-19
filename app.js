cad673e5f577ba0f9ec90b7cd4963ef89eaad566
Yurick <yurick.hauschild@gmail.com>
2018-06-19 17:18:00 -0300

    ♻️  Clean up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import 'normalize.css/normalize.css'
 
 import './base/code.css'
-import './style.css'
+import './base/unclassed.css'
 
 import EventCover from './components/event-cover'
 import ControlPanel from './components/control-panel'
