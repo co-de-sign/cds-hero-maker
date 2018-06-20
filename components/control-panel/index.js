4f2717a8f8e5f2154361d815233cd29fba48597f
Yurick <yurick.hauschild@gmail.com>
2018-06-19 22:40:15 -0300

    ✨ Make indent great again

--- components/control-panel/index.js
+++ components/control-panel/index.js
@@ -13,6 +13,8 @@ export default () => {
   const panel = document.querySelector('.control-panel')
   const select = panel.querySelector('select')
   const logo = document.querySelector('.logo')
+  const edition = panel.querySelector('.number > input')
+  const description = panel.querySelector('.body > textarea')
 
   select.onchange = (event) => {
     const newColor = event.currentTarget.value
@@ -22,5 +24,27 @@ export default () => {
     logo.setAttribute('fill', refColor[newColor])
 
     square.style.backgroundColor = newColor
+  }
+
+  edition.onkeyup = (event) => {
+    const value = event.currentTarget.value
+
+    const code = document.querySelector('.event-cover > .info > code')
+
+
+    code.innerHTML = PR.prettyPrintOne(`.co-de-sign__${value} {
+  ${description.value}
+}`)
+  }
+
+  description.onkeyup = (event) => {
+    const value = event.currentTarget.value
+
+    const code = document.querySelector('.event-cover > .info > code')
+
+    code.innerHTML = PR.prettyPrintOne(
+`.co-de-sign__${edition.value} {
+  ${value}
+}`)
   }
 }
